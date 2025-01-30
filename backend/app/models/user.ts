bf43fe329b04b28154e6b2f1550d7144ee2a0c46
P3T0o <quentin.le-noir@epitech.eu>
2025-01-30 18:25:14 +0100

    [TEST] Test

--- backend/app/models/user.ts
+++ backend/app/models/user.ts
@@ -14,6 +14,7 @@ export default class User extends compose(BaseModel, AuthFinder) {
   @column({ isPrimary: true })
   declare id: number
 
+  // test
   @column()
   declare fullName: string | null
 
