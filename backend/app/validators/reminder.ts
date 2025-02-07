762c72c02d0953d0744b7e009a99a3e496532c37
P3T0o <quentin.le-noir@epitech.eu>
2025-02-07 16:44:01 +0100

    [ADD] Controller User - deleteMe - updateMe - me

--- backend/app/validators/reminder.ts
+++ backend/app/validators/reminder.ts
@@ -3,7 +3,7 @@ import vine from '@vinejs/vine'
 export const UpdateReminderValidator = vine.compile(
   vine.object({
     title: vine.string().trim().minLength(3).maxLength(255).optional(),
-    description: vine.string().trim().optional(),
+    description: vine.string().trim().minLength(3).maxLength(255).optional(),
     finish: vine.boolean().optional(),
     notifiedMail: vine.boolean().optional(),
     dateEnd: vine.date().optional(),
--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -23,7 +23,10 @@ router
       .group(() => {
         router.post('/register', '#controllers/auth_controller.register')
         router.post('/login', '#controllers/auth_controller.login')
+        router.get('/me', '#controllers/auth_controller.me').use(middleware.auth())
+        router.delete('/me', '#controllers/auth_controller.deleteMe').use(middleware.auth())
         router.post('/logout', '#controllers/auth_controller.logout').use(middleware.auth())
+        router.patch('/me', '#controllers/auth_controller.updateMe').use(middleware.auth())
       })
       .prefix('auth')
     // Routes USERS
@@ -40,9 +43,15 @@ router
           .post('/', '#controllers/reminders_controller.userCreateReminder')
           .use(middleware.auth())
         router.get('/me', '#controllers/reminders_controller.myReminders').use(middleware.auth())
-        router.get('/:id', '#controllers/reminders_controller.showMyReminder').use(middleware.auth())
-        router.patch('/:id', '#controllers/reminders_controller.updateMyReminder').use(middleware.auth())
-        router.delete('/:id','#controllers/reminders_controller.destroyMyReminder').use(middleware.auth())
+        router
+          .get('/:id', '#controllers/reminders_controller.showMyReminder')
+          .use(middleware.auth())
+        router
+          .patch('/:id', '#controllers/reminders_controller.updateMyReminder')
+          .use(middleware.auth())
+        router
+          .delete('/:id', '#controllers/reminders_controller.destroyMyReminder')
+          .use(middleware.auth())
       })
       .prefix('reminders')
   })
